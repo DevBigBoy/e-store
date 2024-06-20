1a3b2613522bc4365959ea7a37c5adf899a2a0a7
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2024-06-20 05:26:00 +0300

    4.2 - Implement Verify Email Action

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,15 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    // define middleware on route more readable than this
+    // For Learning To show different method of define midlleware
+    // public function __construct()
+    // {
+    //     // $this->middleware(['auth'])->except(['index','another action name',.....]);
+    //     // $this->middleware(['auth','another middleware'])->except(['index']);
+    //     $this->middleware(['auth'])->only('index');
+    // }
+
     public function index()
     {
         return view('dashboard.index');
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+    <a href="{{ route('dashboard') }}" class="brand-link">
+        <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">Shezo Store</span>
     </a>
@@ -14,7 +14,14 @@
                 <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                <a href="{{ route('profile.edit') }}" class="d-block">{{ Auth::user()->name }}</a>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                        this.closest('form').submit();"
+                        class="d-block">Logout</a>
+                </form>
             </div>
         </div>
 
@@ -47,15 +54,15 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link ">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Active Page</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="#" class="nav-link active">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Inactive Page</p>
+                                <p>Products</p>
                             </a>
                         </li>
                     </ul>
@@ -64,7 +71,7 @@
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-th"></i>
                         <p>
-                            Simple Link
+                            Offers
                             <span class="right badge badge-danger">New</span>
                         </p>
                     </a>
